f7988c3f4ca5090d3fc938a0e44e990b184c63cc
Luke Dyson <luke.dyson@bskyb.com>
2015-12-22 09:06:16 +0000

    Lesson 4: Using standard HTML5 validation on the form fields, and ensuring that the Angular controller only captures a valid submission

--- section1/lesson4/main.js
+++ section1/lesson4/main.js
@@ -1,18 +1,20 @@
-var app = angular.module('minmax', []);
+'use strict';
 
+var app = angular.module('Registration', []);
 
-app.controller('MinMaxCtrl', function ($scope, $http) {
+app.controller('RegistrationControl', function($scope, $http) {
 	$scope.formModel = {};
 
-	$scope.onSubmit = function () {
-		console.log("Hey i'm submitted!");
-		console.log($scope.formModel);
+	$scope.onSubmit = function() {
+		console.log('SUBMIT!',$scope.formModel);
 
-		$http.post('https://minmax-server.herokuapp.com/register/', $scope.formModel).
-			success(function (data) {
-				console.log(":)")
-			}).error(function(data) {
-				console.log(":(")
+		$http.post('https://minmax-server.herokuapp.com/register/', $scope.formModel)
+			.success(function(data) {
+				console.log('Success...',data);
+			})
+			.error(function(data) {
+				// to get this to error with the above endpoint, the name needs to be "error" :-/
+				console.log('Error...',data);
 			});
 	};
 });
