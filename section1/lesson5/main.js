08f042e1e68be82d1e21707101186e0803058741
Luke Dyson <luke.dyson@bskyb.com>
2015-12-22 09:24:34 +0000

    Lesson 5: Angular capturing the HTML5 form validation, and adding CSS classes to the required form fields based on their user input and validation state

--- section1/lesson5/main.js
+++ section1/lesson5/main.js
@@ -1,18 +1,27 @@
-var app = angular.module('minmax', []);
+'use strict';
 
+var app = angular.module('Registration', []);
 
-app.controller('MinMaxCtrl', function ($scope, $http) {
+app.controller('RegistrationControl', function($scope, $http) {
 	$scope.formModel = {};
 
-	$scope.onSubmit = function () {
-		console.log("Hey i'm submitted!");
-		console.log($scope.formModel);
+	$scope.onSubmit = function() {
+		var regForm = $scope.registrationForm;
 
-		//$http.post('https://minmax-server.herokuapp.com/register/', $scope.formModel).
-		//	success(function (data) {
-		//		console.log(":)")
-		//	}).error(function(data) {
-		//		console.log(":(")
-		//	});
+		if (!regForm.$valid) {
+			console.log('the form was invalid; rejecting...');
+			return;
+		}
+
+		console.log('SUBMIT!',$scope.formModel);
+
+		$http.post('https://minmax-server.herokuapp.com/register/', $scope.formModel)
+			.success(function(data) {
+				console.log('Success...',data);
+			})
+			.error(function(data) {
+				// to get this to error with the above endpoint, the name needs to be "error" :-/
+				console.log('Error...',data);
+			});
 	};
 });
