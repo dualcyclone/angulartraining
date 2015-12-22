71443d8a7cc256870d6d6b9ad691e07cc4964a40
Luke Dyson <luke.dyson@bskyb.com>
2015-12-22 12:20:35 +0000

    Lesson 6: Adding in the angular auto validate library for easy form validation; and also adding in custom validation error messages

--- section1/lesson6/main.js
+++ section1/lesson6/main.js
@@ -1,26 +1,32 @@
-var app = angular.module('minmax', []);
+'use strict';
 
+var app = angular.module('Registration', [
+	'jcs-autoValidate' // Adding dependency: angular-auto-validate
+]);
 
+// Adding additional error messages to make the form validation messages user friendly
+app.run(function(defaultErrorMessageResolver){
+	defaultErrorMessageResolver.getErrorMessages()
+		.then(function(errorMessages) {
+			errorMessages['tooYoung'] = 'You need to be at least {0} years of age';
+			errorMessages['tooOld'] = 'You need to be no more than {0} years of age';
+			errorMessages['badUsername'] = 'Your username must be between 7 and 15 characters, and only contain letters (upper or lower case), numbers and underscore';
+		})
+});
 
-
-app.controller('MinMaxCtrl', function ($scope, $http) {
+app.controller('RegistrationControl', function($scope, $http) {
 	$scope.formModel = {};
 
-	$scope.onSubmit = function (valid) {
+	$scope.onSubmit = function() {
+		console.log('SUBMIT!',$scope.formModel);
 
-		if (valid) {
-			console.log("Hey i'm submitted!");
-			console.log($scope.formModel);
-
-			//$http.post('https://minmax-server.herokuapp.com/register/', $scope.formModel).
-			//	success(function (data) {
-			//		console.log(":)")
-			//	}).error(function(data) {
-			//		console.log(":(")
-			//	});
-		} else {
-			console.log("Invalid Form!")
-		}
-
+		$http.post('https://minmax-server.herokuapp.com/register/', $scope.formModel)
+			.success(function(data) {
+				console.log('Success...',data);
+			})
+			.error(function(data) {
+				// to get this to error with the above endpoint, the name needs to be "error" :-/
+				console.log('Error...',data);
+			});
 	};
 });
