cb96ec3c8895332e9853e4855d28a4814588eaba
Luke Dyson <luke.dyson@bskyb.com>
2015-12-22 08:51:25 +0000

    Lesson 3: Submitting a form using $http

--- section1/lesson3/main.js
+++ section1/lesson3/main.js
@@ -1,3 +1,20 @@
-var app = angular.module('minmax', []);
+'use strict';
 
-// https://minmax-server.herokuapp.com/register/'
+var app = angular.module('Registration', []);
+
+app.controller('RegistrationControl', function($scope, $http) {
+    $scope.formModel = {};
+
+    $scope.onSubmit = function() {
+        console.log('SUBMIT!',$scope.formModel);
+
+        $http.post('https://minmax-server.herokuapp.com/register/', $scope.formModel)
+            .success(function(data) {
+                console.log('Success...',data);
+            })
+            .error(function(data) {
+                // to get this to error with the above endpoint, the name needs to be "error" :-/
+                console.log('Error...',data);
+            });
+    };
+});
