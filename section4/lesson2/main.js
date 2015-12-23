53f0a79c6ab67b2641bb581c6238a4ff64a37b79
Luke Dyson <luke.dyson@bskyb.com>
2015-12-23 14:31:55 +0000

    Session 4 | Lesson 2: Root scope; understanding its use - using it as a potential passing place between controllers that may have a sibling / parent-child relationship? Use with caution

--- section4/lesson2/main.js
+++ section4/lesson2/main.js
@@ -1,15 +1,15 @@
 var app = angular.module('codecraft', []);
 
-app.controller('ParentController', function ($scope) {
-	$scope.name = "Parent";
+app.controller('ParentController', function ($scope, $rootScope) {
+	//$scope.name = 'Parent';
+    //
+	//$scope.reset = function () {
+	//	$scope.name = 'Parent';
+	//};
+});
 
+app.controller('ChildController', function ($scope, $rootScope) {
 	$scope.reset = function () {
-		$scope.name = "Parent";
+		$rootScope.name = 'Reset by child';
 	};
-
 });
-
-app.controller('ChildController', function ($scope) {
-});
-
-
