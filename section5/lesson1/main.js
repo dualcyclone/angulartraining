1589d23580069255bafbea269b0b61aded95c200
Luke Dyson <luke.dyson@bskyb.com>
2015-12-23 15:38:41 +0000

    Section 5 | Lessons 2-5: Adding a service to manage contacts between the contexts. Extended on tutorial by continuing to load in the persons.json, keeping the select/deselect person functionality

--- section5/lesson1/main.js
+++ section5/lesson1/main.js
@@ -1,5 +1,34 @@
 var app = angular.module('codecraft', []);
 
+app.service('ContactService', function($http, $q) {
+	var deffered = $q.defer(),
+		service = {
+			'addPerson': function (person) {
+				this.persons.push(person);
+			},
+			'selectedPerson': null,
+			'selectPerson': function(person) {
+				var selectedPerson = person,
+					isSameAsSelected = (service.selectedPerson !== selectedPerson);
+
+				// If the selected person is the same as the one already selected, de-select it, otherwise persist the person data
+				service.selectedPerson = isSameAsSelected ? selectedPerson : null;
+			},
+			'persons': []
+		};
+
+	service.async = function() {
+		$http.get('persons.json')
+			.then(function(res){
+				service.persons = res.data;
+				deffered.resolve();
+			});
+		return deffered.promise;
+	};
+
+	return service;
+});
+
 // Proper custom filter for searching
 app.filter('sensitiveSearch', function() {
 	return function(persons, search) {
@@ -10,27 +39,16 @@ app.filter('sensitiveSearch', function() {
 	};
 });
 
-app.controller('PersonListController', function($scope, $rootScope, $http, $filter) {
-	$http.get('persons.json')
-		.then(function(res){
-			$scope.persons = res.data;
-		});
-
+app.controller('PersonListController', function($scope, $filter, ContactService) {
 	$scope.search = ''; // initialise the search value
-	$rootScope.selectedPerson = null; // initialise the selected person data of the row (currently none selected)
 	$scope.sorting = ''; // initialise as no predefined order - this will also map to the "Select order" option in the form
+	$scope.contacts = ContactService;
 
-	$scope.selectPerson = function() {
-		var selectedPerson = this.person,
-			isSameAsSelected = ($rootScope.selectedPerson !== selectedPerson);
-
-		// If the selected person is the same as the one already selected, de-select it, otherwise persist the person data
-		$rootScope.selectedPerson = isSameAsSelected ? selectedPerson : null;
-	};
+	ContactService.async(); // collect the persons data
 
 	// helper to find if the selected person exists in the filtered subset
 	$scope.selectedPersonExists = function() {
-		return $scope.filteredPersons.indexOf($rootScope.selectedPerson) > -1;
+		return $scope.filteredPersons.indexOf($scope.selectedPerson) > -1;
 	};
 
 	$scope.sortBy = function(sorting) {
@@ -39,17 +57,17 @@ app.controller('PersonListController', function($scope, $rootScope, $http, $filt
 	};
 
 	$scope.filterPersons = function() {
-		var filtered = $scope.persons ? $filter('sensitiveSearch')($filter('orderBy')($scope.persons, $scope.sorting), $scope.search) : []; // $scope.persons is undefined on initial load... http get to slow?
+		var filtered = $filter('sensitiveSearch')($filter('orderBy')($scope.contacts.persons, $scope.sorting), $scope.search); // $scope.persons is undefined on initial load... http get to slow?
 
 		// If the selected person no longer exists within the filtered persons subset, reset the selected person
-		if (filtered.indexOf($rootScope.selectedPerson) === -1) {
-			$rootScope.selectedPerson = null;
+		if (filtered.indexOf($scope.contacts.selectedPerson) === -1) {
+			$scope.contacts.selectedPerson = null;
 		}
 
 		return filtered;
 	};
 });
 
-app.controller('PersonDetailController', function($scope) {
-
+app.controller('PersonDetailController', function($scope, ContactService) {
+	$scope.contacts = ContactService;
 });
