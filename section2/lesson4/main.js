ad9ff8dcd85c16daab2f234a88375f4859c7aed6
Luke Dyson <luke.dyson@bskyb.com>
2015-12-23 09:12:41 +0000

    Section 2 | Lesson 4: changing the search field to allow for mixed data, and using a custom filter to check if the search term exists at the start of either the name or the email (case sensitivity included). Extended the tutorial to ensure that performance is improved by using the "track by" statement in the ng-repeat loop for the data to prevent DOM rebuilding when unnecessary. Also added persistence to ensure that any previously selected person information is hidden if that person no longer exists in the filtered persons subset.

--- section2/lesson4/main.js
+++ section2/lesson4/main.js
@@ -6,20 +6,49 @@ app.controller('PersonController', function($scope, $http) {
 			$scope.persons = res.data;
 		});
 
-	$scope.search = {}; // initial
+	var searchPersistence = '',// Search query text persistence, to check for changes between searches to aid subset persistence
+		filteredPersons = []; // Persistence of found persons based on the query text. Initialised as empty, but angular will populate this immediate on load
 
-	$scope.selectedIndex = -1; // initialise the selected index of the row (currently none selected)
+	$scope.search = ''; // initialise the search value
 	$scope.selectedPerson = null; // initialise the selected person data of the row (currently none selected)
 	$scope.selectPerson = function() {
 		var selectedPerson = this.person,
-			index = this.$index,
-			isSameAsSelected = ($scope.selectedIndex !== index);
-
-		// If the selected index is not the same as what we have just clicked, set the selected index; if it is the same, reset it
-		$scope.selectedIndex = isSameAsSelected ? index : -1;
+			isSameAsSelected = ($scope.selectedPerson !== selectedPerson);
 
 		// If the selected person is the same as the one already selected, de-select it, otherwise persist the person data
 		$scope.selectedPerson = isSameAsSelected ? selectedPerson : null;
 	};
 
+	// helper to find if the selected person exists in the filtered subset
+	$scope.selectedPersonExists = function() {
+		return filteredPersons.indexOf($scope.selectedPerson) > -1;
+	};
+
+	// Custom filter for filtering a set of data
+	$scope.sensitiveSearch = function(person, index) {
+		var search = $scope.search,
+
+			// If there is a query, perform the real filter; otherwise always return true to prevent any filtering of each person
+			found = (search !== '' ? (person.name.indexOf(search) === 0 || person.email.indexOf(search) === 0) : true);
+
+		// If the new search term isn't the same as the old search term, clear the persisted filtered persons subset
+		if (search !== searchPersistence) {
+			filteredPersons = [];
+		}
+
+		// If this person matches the search query, and the person doesn't already exist in the subset, push them into the filtered persons subset
+		if (found && filteredPersons.indexOf(person) === -1) {
+			filteredPersons.push(person);
+		}
+
+		// Persist the search term (regardless of whether it has changed or not)
+		searchPersistence = search;
+
+		// If the selected person no longer exists within the filtered persons subset, reset the selected person
+		if (index === $scope.persons.length-1 && filteredPersons.indexOf($scope.selectedPerson) === -1) {
+			$scope.selectedPerson = null;
+		}
+
+		return found;
+	}
 });
