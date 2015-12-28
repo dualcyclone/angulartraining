9d4c0d7b28f01846c1590b482855b4ccd7939b10
Luke Dyson <luke.dyson@flyinganvil.co.uk>
2015-12-28 10:46:36 +0000

    Session 6 | Lession 19: Adding the bs-datepicker to the birthday field, adding the bootstrap additions to style the datepicker, and also updated its defaults to use the UK date format

--- section6/lesson1/main.js
+++ section6/lesson1/main.js
@@ -7,13 +7,18 @@ var app = angular.module('codecraft', [
     'mgcrea.ngStrap'
 ]);
 
-app.config(function($httpProvider, $resourceProvider, laddaProvider) {
+app.config(function($httpProvider, $resourceProvider, laddaProvider, $datepickerProvider) {
 	$httpProvider.defaults.headers.common['Authorization'] = 'Token da4eb42da465bc8d3334f33b4d16fd68d4882885';
 	$resourceProvider.defaults.stripTrailingSlashes = false;
 
     laddaProvider.setOption({
         style: 'expand-right'
-    })
+    });
+
+    angular.extend($datepickerProvider.defaults, {
+        dateFormat: 'd/M/yyyy',
+        autoclose: true
+    });
 });
 
 app.factory('Contact', function($resource) {
