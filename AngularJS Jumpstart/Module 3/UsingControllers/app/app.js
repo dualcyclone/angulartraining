ef2a65e717b91dc71cd2b0b278fa5d2d1fcfe820
Luke Dyson <luke.dyson@flyinganvil.co.uk>
2016-01-03 11:04:44 +0000

    Modularising the app

---
+++ AngularJS Jumpstart/Module 3/UsingControllers/app/app.js
@@ -0,0 +1,4 @@
+(function(){
+    // Initialising our module / app - creates the app in the angular scope, and not the global scope
+    angular.module('customerApp', []);
+}());
--- AngularJS Jumpstart/Module 3/UsingControllers/app/controllers/CustomerController.js
+++ AngularJS Jumpstart/Module 3/UsingControllers/app/controllers/CustomerController.js
@@ -1,18 +1,28 @@
-var demoApp = angular.module('demoApp', []);
+(function(){
+    // When referencing our module via angular, we do not need to inject any dependencies, hence the lack of [].
+    var app = angular.module('customerApp');
 
-demoApp.controller('CustomerController', function ($scope) {
-    $scope.customerSorting = '';
-    $scope.reverse = false;
+    // Defining our controller
+    var CustomerController = function ($scope) {
+        $scope.customerSorting = '';
+        $scope.reverse = false;
 
-    $scope.customers = [
-        {joined: '2000-12-02', name:'John', city:'Chandler',  orderTotal: 9.9956},
-        {joined: '1965-01-25', name:'Zed',  city:'Las Vegas', orderTotal: 19.99},
-        {joined: '1944-06-15', name:'Tina', city:'New York',  orderTotal:44.99},
-        {joined: '1995-03-28', name:'Dave', city:'Seattle',   orderTotal:101.50}
-    ];
+        $scope.customers = [
+            {joined: '2000-12-02', name:'John', city:'Chandler',  orderTotal: 9.9956},
+            {joined: '1965-01-25', name:'Zed',  city:'Las Vegas', orderTotal: 19.99},
+            {joined: '1944-06-15', name:'Tina', city:'New York',  orderTotal:44.99},
+            {joined: '1995-03-28', name:'Dave', city:'Seattle',   orderTotal:101.50}
+        ];
 
-    $scope.doSort = function(propName) {
-        $scope.customerSorting = propName;
-        $scope.reverse = !$scope.reverse;
+        $scope.doSort = function(propName) {
+            $scope.customerSorting = propName;
+            $scope.reverse = !$scope.reverse;
+        };
     };
-});
+
+    // if using minification, using this assignment will fix the dependency injection when the argument names are minified
+    //CustomerController.$inject = ['$scope']; // needs to be in the exact same order as the actual function arguments
+
+    // Hook the controller to the controller reference in the app
+    app.controller('CustomerController', CustomerController);
+}());
